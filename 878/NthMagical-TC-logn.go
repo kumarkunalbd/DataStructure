96ef4281ad87a5179b5ecbafb544f39a42f2c0e0
kumarkunalbd <kumar.kunal.bd@gmail.com>
2023-09-10 17:01:22 +0530

    TC is log n

---
+++ 878/NthMagical-TC-logn.go
@@ -0,0 +1,34 @@
+func nthMagicalNumber(n int, a int, b int) int {
+	m := 1000000007
+	gcd := getGCD(a, b)
+	lcm := (a / gcd) * b
+
+	s := 0
+	e := n * int(math.Min(float64(a), float64(b)))
+
+	for s < e {
+		mid := (s + e) / 2
+
+		if (mid/a + mid/b - mid/lcm) < n {
+			s = mid + 1
+		} else {
+			e = mid
+		}
+	}
+
+	return int(s % m)
+
+}
+
+func getGCD(a, b int) int {
+	// base vase
+	if a == 0 {
+		return b
+	}
+	// main logic
+	if a < b {
+		a, b = b, a
+	}
+	gcd := getGCD(a%b, b)
+	return gcd
+}
