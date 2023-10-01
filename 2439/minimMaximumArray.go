39e42dfa9392e8028da79c632941b84ab20f231d
kumarkunalbd <kumar.kunal.bd@gmail.com>
2023-10-01 12:11:22 +0530

    Tc- n

--- 2439/minimMaximumArray.go
+++ 2439/minimMaximumArray.go
@@ -1,42 +1,14 @@
 func minimizeArrayValue(nums []int) int {
-	s := math.MaxInt
-	e := math.MinInt
-
-	for _, val := range nums {
-		if val < s {
-			s = val
-		}
-		if val > e {
-			e = val
+	curSum := 0
+	minMax := math.MinInt
+	for i, val := range nums {
+		curSum += val
+		curAvg := int(math.Ceil(float64(curSum) / float64(i+1)))
+		//fmt.Printf("i=%v val=%v curAvg=%v minMax=%v\n",i,val,curAvg,minMax)
+		if curAvg > minMax {
+			minMax = curAvg
 		}
 	}
 
-	ans := e
-	for s <= e {
-		mid := (s + e) / 2
-		fmt.Printf("l1: s=%v mid=%v e=%v\n", s, mid, e)
-		if canMaximumValueBe(nums, mid) {
-			ans = mid
-			e = mid - 1
-		} else {
-			s = mid + 1
-		}
-		fmt.Printf("l2: s=%v mid=%v e=%v ans=%v\n", s, mid, e, ans)
-	}
-	return ans
-}
-
-func canMaximumValueBe(nums []int, target int) bool {
-	if nums[0] > target {
-		return false
-	}
-	prevMinPossible := nums[0]
-	for i := 1; i < len(nums); i++ {
-		curMinPoss := nums[i] - (target - prevMinPossible)
-		if curMinPoss > target {
-			return false
-		}
-		prevMinPossible = curMinPoss
-	}
-	return true
+	return minMax
 }
