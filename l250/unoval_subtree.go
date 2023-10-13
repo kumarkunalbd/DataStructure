b6474496a1f51dea1bbc9f09c1879c6caae20c94
kumarkunalbd <kumar.kunal.bd@gmail.com>
2023-10-13 22:46:46 +0530

    solved TC-n

---
+++ l250/unoval_subtree.go
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package l250
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countUnivalSubtrees(root *TreeNode) int {
+	ans, _ := univalSubtrees(root)
+	return ans
+}
+
+func univalSubtrees(root *TreeNode) (int, bool) {
+	// base case
+	if root == nil {
+		return 0, true
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return 1, true
+	}
+
+	// main logic
+	lVal, isLU := univalSubtrees((*root).Left)
+	rVal, isRU := univalSubtrees((*root).Right)
+	if !isLU || !isRU {
+		return lVal + rVal, false
+	}
+
+	if root.Left != nil && root.Right == nil {
+		if root.Val == root.Left.Val {
+			return lVal + 1, true
+		}
+		return lVal, false
+	}
+	if root.Right != nil && root.Left == nil {
+		if root.Right.Val == root.Val {
+			return rVal + 1, true
+		}
+		return rVal, false
+	}
+
+	if root.Val == root.Left.Val && root.Val == root.Right.Val {
+		return lVal + rVal + 1, true
+	}
+	return lVal + rVal, false
+}
