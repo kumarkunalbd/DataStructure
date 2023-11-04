749cf65235e0be4996f88ddf6a9474b0d1496bce
kumarkunalbd <kumar.kunal.bd@gmail.com>
2023-11-04 10:11:50 +0530

    TC- logn

---
+++ l270/closest_val_bst.go
@@ -0,0 +1,41 @@
+package l270
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func closestValue(root *TreeNode, target float64) int {
+	ans, _ := closestValueWithDiff(root, target)
+
+	return ans
+}
+
+func closestValueWithDiff(root *TreeNode, target float64) (int, float64) {
+	// base case
+	if root == nil {
+		return math.MaxInt, math.MaxFloat64
+	}
+	// main logic
+	curDiff := math.Abs(float64(root.Val) - target)
+	closestVal := root.Val
+	if float64(root.Val) > target {
+		lClose, lDiff := closestValueWithDiff(root.Left, target)
+		if lDiff <= curDiff {
+			curDiff = lDiff
+			closestVal = lClose
+		}
+	} else if float64(root.Val) < target {
+		rClose, rDiff := closestValueWithDiff(root.Right, target)
+		if rDiff < curDiff {
+			curDiff = rDiff
+			closestVal = rClose
+		}
+	}
+	return closestVal, curDiff
+}
